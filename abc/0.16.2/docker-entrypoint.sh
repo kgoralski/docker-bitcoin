a03068979b8dabe0a958bd81b4b59902625fd502
Krzysztof Góralski <krzysztof.goralski@bitgamelabs.com>
2018-01-25 12:22:17 +0100

    zeromq ip fix

--- abc/0.16.2/docker-entrypoint.sh
+++ abc/0.16.2/docker-entrypoint.sh
@@ -4,21 +4,19 @@ set -e
 if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1" == "test_bitcoin" ]]; then
 	mkdir -p "$BITCOIN_DATA"
 
-	if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
-		cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
+	cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
 		server=1
 		rest=1
 		printtoconsole=1
 		rpcallowip=::/0
 		rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
 		rpcuser=${BITCOIN_RPC_USER:-bitcoin}
-		zmqpubhashblock=tcp://127.0.0.1:28332
-		zmqpubhashtx=tcp://127.0.0.1:28332
-		zmqpubrawblock=tcp://127.0.0.1:28332
-		zmqpubrawtx=tcp://127.0.0.1:28332
+		zmqpubhashblock=tcp://0.0.0.0:28332
+		zmqpubhashtx=tcp://0.0.0.0:28332
+		zmqpubrawblock=tcp://0.0.0.0:28332
+		zmqpubrawtx=tcp://0.0.0.0:28332
 		EOF
-		chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
-	fi
+	chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
 
 	# ensure correct ownership and linking of data directory
 	# we do not update group ownership here, in case users want to mount
